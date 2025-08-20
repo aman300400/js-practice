2071bbd95f5e08d0fcb9e8b5ce28a1670dd8b7f7
Aman Kumar <110847260+aman300400@users.noreply.github.com>
2025-08-20 11:08:51 +0530

    Add files via upload

---
+++ nonPrimitives.js
@@ -0,0 +1,14 @@
+const username = {
+  firstname: "aman"
+  isLoggedIn: true
+}
+
+const myFirstName = "aman"
+myFirstName = "Mr. A"
+
+console.log(myFirstName);
+
+console.log(username);
+console.log(typeof username);
+
+// ldg nzmd ==s oviding 
---
+++ premitives.js
@@ -0,0 +1,43 @@
+// numbers 
+
+
+
+
+// to tun node   node part2/premitives
+// let balance = 120;
+// let anotherbalance =   new Number(120);
+
+// console.log(balance)
+
+// console.log(typeof (balance));
+// console.log(typeof anotherbalance)
+
+// boolean
+// let isActive = true ;
+// let isReallyActive = new Boolean(true);  // not good but still
+
+// // null and undedfind
+
+// let firstname = null
+// let lastname  = undefined
+// console.log(firstname);
+// console.log(lastname);
+
+// // string
+// let mystring = "hello"
+// let mystringone = "hola"
+// let username = "AMAN"
+
+
+// let oldgreet = mystring + " " + username + " how are you "; // old way of writing a string
+
+// let greetMessage = `hello  ${username} how are you `// new way of writng a string
+
+
+// let demoOne = `the price of the product is  is $(2*88)`
+// console.log(demoOne);
+
+let sm1 = Symbol();
+let sm2 = Symbol();
+
+console.log(sm1 == sm2);
