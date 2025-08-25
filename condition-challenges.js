c9cebd43048dbc68c1dac286a19c957b2344382d
Aman Kumar <110847260+aman300400@users.noreply.github.com>
2025-08-25 22:37:18 +0530

    Add files via upload

---
+++ condition-challenges.js
@@ -0,0 +1,60 @@
+//check if a number is greater then another numbr
+
+
+
+
+// let  num1 = 14
+// let  num2 = 55
+// console.log("i am just a regular uper code");
+
+// if (num1 < num2) {
+//   console.log(" the num1 is greater then num2 as per calculations");} //haa haa prettyfunny right
+
+//   console.log(" i am just a regular bottom code");
+  
+  
+//  node jsfoundation/part3/condition-challenges.js
+
+
+// check if it is equal to another strng or not
+
+let username = "chai"
+let anotherUsername = "chaii"
+
+if (username == anotherUsername){
+// console.log("pick another username ");
+  }
+  else{
+   // console.log("you can pick this username");
+    
+  }
+
+  // checking  if anvariable is a number ornot
+
+  let score = 100;  if (typeof score === "number"){
+    console.log("yep this is a number");
+  }
+  else {
+    console.log("you can pick this user name oh yeah!!");
+  }
+
+
+  // check if abookean value is true or false
+
+  let isteaready = false 
+  if (isteaready)
+{
+  console.log("tea is ready")
+}
+else{
+  //console.log("tea is not ready");
+}
+
+// check if arrayis empty or not
+
+let items = ["item1"]
+console.log(items.length);
+
+if(items.length === 0 ){
+  console.log("array is not empty")
+} 
